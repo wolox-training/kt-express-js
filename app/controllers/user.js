a0e05fd771b5a28c6b664082eaac21ee981f32c1
KevinTemes <kevintemes@gmail.com>
2018-01-02 08:32:16 -0300

    signup functionality

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,39 @@
 const userModel = require('../models/user'),
-  errors = require('../errors');
+  errors = require('../errors'),
+  path = require('path');
 
+
+exports.signup = (req, res, next) => {
+
+  res.status(200);
   
+  res.sendFile(path.resolve('app/views/user/signup.html'));
+
+};
+
+exports.create = (req, res, next) => {
+  
+  res.status(200);
+
+  let validation = validateInput(req.body);
+
+  res.send('OK!');
+  
+};
+
+const validateInput = (input) => {
+
+  let errors = null;
+
+  input.forEach(element => {
+    
+    if(typeof(element) == 'undefined' || element === ''){
+      
+      errors = 'All fields are required!';
+      
+      return errors;
+    }
+
+  });
+
+};
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
-// const controller = require('./controllers/controller');
+const user = require('./controllers/user');
 
 exports.init = (app) => {
 
+  app.get('/signup', [], user.signup);
+  app.post('/signup', [], user.create);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
