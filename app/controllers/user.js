87a052cbac9db625e8670737a121052498754746
KevinTemes <kevintemes@gmail.com>
2018-01-05 08:55:28 -0300

    pr fixes in controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,4 @@
 const User = require('../models').users,
-  errors = require('../errors'),
   logger = require('../logger');
 
 exports.create = (req, res, next) => {
@@ -21,7 +20,7 @@ exports.create = (req, res, next) => {
     if(error.errors){
       errorBag = error.errors.map(err => err.message);
       logger.error(`A database error occured when attempting a user signup. Details: ${errorBag}.`);
-      res.status(401).send(errorBag);
+      res.status(400).send(errorBag);
     }else{
       logger.error(`Unhandled error! details: ${error}`);
       res.status(500).send(error);
