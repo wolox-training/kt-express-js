8ac4706d0253003d6d8edcb50afc889e84f5d73c
KevinTemes <kevintemes@gmail.com>
2018-01-08 11:56:57 -0300

    Signup (#6)
    * signup functionality
    
    * input validation added to signup
    
    * signup module finalized
    
    * PR fixes
    
    * more pr fixes
    
    * pr fixes in controller
    
    * merged development into signup; travis file name fixed
    
    * invalid travis file deleted
    
    * travis file fix

---
+++ app/controllers/user.js
@@ -0,0 +1,45 @@
+const User = require('../models').users,
+  logger = require('../logger');
+
+exports.create = (req, res, next) => {
+
+  const input = emptyToNull(req.body);
+
+  User.create({
+    name: input.name,
+    lastName: input.lastName,
+    email: input.email,
+    password: input.password
+  }).then(result => {
+    logger.info(`User ${input.name} created successfully.`);
+    res.status(200).send(`User ${input.name} created successfully.`);
+
+  }).catch(error => {
+
+    let errorBag = [];
+    if(error.errors){
+      errorBag = error.errors.map(err => err.message);
+      logger.error(`A database error occured when attempting a user signup. Details: ${errorBag}.`);
+      res.status(400).send(errorBag);
+    }else{
+      logger.error(`Unhandled error! details: ${error}`);
+      res.status(500).send(error);
+    }
+
+  });
+  
+};
+
+const emptyToNull = (input) => {
+
+  let newInput = {};
+
+  Object.keys(input).forEach((key) => {
+
+    newInput[key] = input[key] === '' ? null : input[key];
+  
+  });
+
+  return newInput;
+
+};
--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,57 @@
 'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('users', {
-    name: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      notEmpty: true
+    },
+    lastName: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      notEmpty: true
+    },
+    password: {
+      type: DataTypes.STRING, 
+      isAlphanumeric: true,
+      allowNull:false,
+      notEmpty: true,
+      validate: {
+        len: {
+          args: [8, 255],
+          msg: 'Passwords must contain at least 8 characters.'
+        }
+      }
+    },
+    email:{
+      type: DataTypes.STRING, 
+      isEmail: true,
+      allowNull:false,
+      unique:true,
+      notEmpty: true,
+      validate: {
+        woloxMail(value) {
+          if (!value.endsWith('@wolox.com.ar') && !value.endsWith('@wolox.com')){
+            throw new Error('The email provided is not a valid Wolox email.');
+          }
+        }
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      },
+    },
+    hooks: {
+      afterValidate: (user, options) => {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(8), null);
       }
     }
   });
   return user;
 };
+
