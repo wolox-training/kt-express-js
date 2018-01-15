f831204f206ab9a8433fdbcb96a94c1dd31ea9ae
KevinTemes <kevintemes@gmail.com>
2018-01-15 09:17:32 -0300

    PR fixes to album_list branch

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,26 +1,7 @@
-const User = require('../models').users,
-  logger = require('../logger'),
-  https = require('https'),
-  config = require('../../config');
+const albumService = require('../services/album');
 
 exports.list = (req, res, next) => {
 
-  logger.info(`Attempting GET request to url ${config.common.urlRequests.albumList}`);
-
-  https.get(config.common.urlRequests.albumList, (resp) => {
-    let data = '';
-    
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-    
-    resp.on('end', () => {
-      logger.info(`Response received from ${config.common.urlRequests.albumList}`);
-      return res.status(200).send(JSON.parse(data));
-    });
- 
-  }).on('error', (err) => {
-    logger.error(`An error occured while attempting a GET request to url ${config.common.urlRequests.albumList}: ${err.message}`);
-  });
+  albumService.listRequest(req, res, next);
 
 };
---
+++ app/services/album.js
@@ -0,0 +1,21 @@
+const rp = require('request-promise'),
+  logger = require('../logger'),
+  config = require('../../config');
+
+exports.listRequest = (req, res, next) => {
+
+  logger.info(`Attempting GET request to url ${config.common.urlRequests.albumList}`);
+
+  rp(config.common.urlRequests.albumList).then(response => {
+
+    logger.info(`Response received from ${config.common.urlRequests.albumList}`);
+    return res.status(200).send(JSON.parse(response));
+
+  }).catch(error => {
+
+    logger.error(`An error occured while attempting a GET request to url ${config.common.urlRequests.albumList}: ${error.message}`);
+    return res.status(502).send(error.message);
+
+  });
+
+};
