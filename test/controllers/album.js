abf7c1dbfb84a8d792a29c3f9a62fc00ee5a8c70
KevinTemes <kevintemes@gmail.com>
2018-01-17 08:56:07 -0300

    Http requests mocked in test cases

--- test/controllers/album.js
+++ test/controllers/album.js
@@ -2,10 +2,14 @@ let chai = require('chai'),
   chaiHttp = require('chai-http'),
   server = require('../../app'),
   should = chai.should(),
+  nock = require('nock'),
+  config = require('../../config'),
   dictum = require('dictum.js'),
   User = require('../../app/models').users;
 
 chai.use(chaiHttp);
+
+const url = config.common.urlRequests.albumList;
 
 const newUser = {
   name: 'Kevin',
@@ -19,9 +23,19 @@ const correctLogin = {
   password: '12345678'
 };
 
+const oneAlbum = {
+  userId: 1, 
+  id: 1, 
+  title: 'quidem molestiae enim'
+};
+
 describe('/GET albums', () => {
 
   it('should successfully retrieve a list of albums', (done) => {
+
+    nock(url)
+      .get('')
+      .reply(200, [oneAlbum]);
   
     User.create(newUser).then(res => {
       chai.request(server)
@@ -33,7 +47,7 @@ describe('/GET albums', () => {
             .set('token', auth.body.token)
             .then(res => {
               res.should.have.status(200);
-              res.body.should.include({userId: 1, id: 1, title: 'quidem molestiae enim'});
+              res.body.should.include(oneAlbum);
               dictum.chai(res, 'Get Albums');
               done();
             });
