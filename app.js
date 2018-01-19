d261a40e4ce5aaa5a829fd19a1c0fb64d3c9cc32
KevinTemes <kevintemes@gmail.com>
2018-01-19 11:25:14 -0300

    Additional port fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const bodyParserUrlencodedConfig = () => ({
 
 const init = () => {
   const app = express();
-  const port = config.common.port;
+  const port = process.env.PORT || config.common.port;
   module.exports = app;
 
   app.use('/docs', express.static(path.join(__dirname, 'docs')));
