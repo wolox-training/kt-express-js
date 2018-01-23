622b216782b0d032d60fa94b13d254c60c4552f9
KevinTemes <kevintemes@gmail.com>
2018-01-23 11:56:43 -0300

    User seeder file (#21)
    * user seeder
    
    * Seeder script modified
    
    * more fixes to seeder file

---
+++ app/models/scripts/seeds.js
@@ -0,0 +1,26 @@
+const User = require('../../models').users,
+  moment = require('moment');
+
+const addUser = (newUser) => {
+
+  return User.findOne({where: {email: newUser.email}}).then(user => {
+    if(!user){
+      return User.create(newUser);
+    }
+    return user.update(newUser);
+  });
+
+};
+
+const users = [
+  addUser({
+    name: 'Kevin',
+    lastName: 'Temes',
+    email: 'kevin.temes@wolox.com.ar',
+    password: '12345678',
+    isAdmin: true,
+    lastInvalidation: moment()
+  })
+];
+
+Promise.all(users).then(() => process.exit(0));
